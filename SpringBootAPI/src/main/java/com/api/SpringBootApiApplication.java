f3f0b8cb1502a5a097005cf458c736a0371fc355
Giuseppe <Giuseppe@DESKTOP-GVG3L3N>
2019-01-25 00:01:12 -0200

    Profiles e properties

--- SpringBootAPI/src/main/java/com/api/SpringBootApiApplication.java
+++ SpringBootAPI/src/main/java/com/api/SpringBootApiApplication.java
@@ -1,14 +1,33 @@
 package com.api;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootApiApplication {
-
+	
+	/*
+	 * Referência do arquivo application.properties
+	@Value("${quantidade.unidade}")
+	private int qtd;
+	*/
+	
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootApiApplication.class, args);//
+		SpringApplication.run(SpringBootApiApplication.class, args);
 	}
+	
+	/*
+	 * Primeiro método a ser chamado após inicialização do método main
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			System.out.println(this.qtd);
+		};
+	}
+	*/
 
 }
 
