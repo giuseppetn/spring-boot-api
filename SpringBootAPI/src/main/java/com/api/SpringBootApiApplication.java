98783481d18e744e613b2bfca2ee6976fa3648fd
Giuseppe <Giuseppe@DESKTOP-GVG3L3N>
2019-01-26 10:53:58 -0200

    Jpa Repository

--- SpringBootAPI/src/main/java/com/api/SpringBootApiApplication.java
+++ SpringBootAPI/src/main/java/com/api/SpringBootApiApplication.java
@@ -1,16 +1,24 @@
 package com.api;
 
-import org.springframework.beans.factory.annotation.Value;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.api.entities.Empresa;
+import com.api.repositories.EmpresaRepository;
+
 @SpringBootApplication
 public class SpringBootApiApplication {
 	
-	@Value("${ambiente}")
-	private String ambiente;
+	/* Essa anotação serve para injetar um componente dentro de outro componente */
+	@Autowired
+	private EmpresaRepository empresaRepository;
+	
 	
 	/*
 	 * Referência do arquivo application.properties
@@ -25,10 +33,30 @@ public class SpringBootApiApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner() {
 		return args -> {
-			System.out.println(this.ambiente);
+			
+			/* Persistindo uma entidade no banco de dados */
+			Empresa empresa = new Empresa();
+			empresa.setRazaoSocial("Empresa 1");
+			empresa.setCnpj("89510341000110");
+			this.empresaRepository.save(empresa);
+						
+			/* Consultando todas as empresas no banco de dados */
+			List<Empresa> empresas = this.empresaRepository.findAll();
+			empresas.forEach(System.out::println);
+			
+			/* Consultando empresas por ID */
+			Optional<Empresa> empresaDB = this.empresaRepository.findById(1L);
+			System.out.println(empresaDB);
+			
+			/* Consultando empresa por CNPJ */
+			Empresa empresaCnpj = this.empresaRepository.findByCnpj("89510341000110");
+			System.out.println("Empresa por CNPJ: " + empresaCnpj);
+			
+			/* Deletando uma empresa por ID */
+			this.empresaRepository.deleteById(1L);
+			
 		};
 	}
-	
 	
 	/*
 	 * Primeiro método a ser chamado após inicialização do método main
