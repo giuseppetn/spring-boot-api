6d758af35af7a1bb35b21f336c51eb7d3308e24c
Giuseppe <Giuseppe@DESKTOP-GVG3L3N>
2019-02-05 20:30:43 -0200

    Ajustes

--- SpringBootAPI/src/main/java/com/api/SpringBootApiApplication.java
+++ SpringBootAPI/src/main/java/com/api/SpringBootApiApplication.java
@@ -1,5 +1,7 @@
 package com.api;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,11 +13,17 @@ import org.springframework.context.annotation.Bean;
 
 import com.api.entities.Empresa;
 import com.api.repositories.EmpresaRepository;
+import com.api.utils.SenhaUtils;
+import com.ibm.watson.developer_cloud.service.security.IamOptions;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifiedImages;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyOptions;
 
 
 @SpringBootApplication
 public class SpringBootApiApplication {
 	
+	private static final String SINGLE_IMAGE_FILE = "C:\\Users\\Giuseppe\\Pictures\\mulher-rabuda-011.jpg";
 	/* Essa anotação serve para injetar um componente dentro de outro componente */
 	@Autowired
 	private EmpresaRepository empresaRepository;
@@ -31,8 +39,30 @@ public class SpringBootApiApplication {
 	private int qtd;
 	*/
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		SpringApplication.run(SpringBootApiApplication.class, args);
+		
+		VisualRecognition service = new VisualRecognition("2018-03-19");
+		
+		//service.setEndPoint("https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify");
+		
+		IamOptions iamOptions = new IamOptions.Builder()
+				  .apiKey("Li24xz63mNzFXEKbHT0ecmoVu0Dv5N_Zqk6wl4taGqWC")
+				  .build();
+		
+		service.setIamCredentials(iamOptions);
+		
+		System.out.println("Classify an image");
+		
+		ClassifyOptions options = new ClassifyOptions.Builder()
+				  .imagesFile(new File(SINGLE_IMAGE_FILE))
+				  .build();
+		
+		ClassifiedImages result = service.classify(options).execute();
+		
+		System.out.println("Resultadooooo: " + result);
+		
+		//System.out.println(SenhaUtils.gerarBCrypt("123456"));
 	}
 	
 	/*
--- SpringBootAPI/src/main/java/com/api/security/config/WebSecurityConfig.java
+++ SpringBootAPI/src/main/java/com/api/security/config/WebSecurityConfig.java
@@ -49,7 +49,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity.csrf().disable().exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
-				.antMatchers("/auth/**").permitAll().anyRequest().authenticated();
+				.antMatchers("/api/exemplo/**").permitAll().anyRequest().authenticated();
 		httpSecurity.addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
 		httpSecurity.headers().cacheControl();
 	}
